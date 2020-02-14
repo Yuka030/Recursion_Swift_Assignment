bf61339cd7d8af06b3fbf625e7905016131cc538
Yuka <yuka.0531.a@gmail.com>
2020-02-13 16:28:07 -0800

    Recursion assignment (swift)

---
+++ Recursion.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+func evaluate(_ str: String)->Int{
+    var str = str.replacingOccurrences(of: " ", with:"")
+
+    //base case
+    if str.count < 2 {
+       return  Int(str) ?? 0
+    }
+    
+    //recursive case
+    var array  = Array(str)
+    array.remove(at: 0)
+    array.remove(at: array.count - 1)
+
+    var i = 0;
+    var count = 0;
+    while(i < array.count){
+        if(array[i] == "("){
+            count += 1
+        }else if(array[i] == ")"){
+            count -= 1
+        }
+
+        if(count == 0){
+            if(array[i] == "+" || array[i] == "*"){
+
+            let left = 6//evaluate(array[0, i])
+            var calc = array[i]
+            let right = 3//evaluate(array[i + 1, array.count])
+            
+            if(calc == "+"){
+                    return left + right
+                }else if(calc == "*"){
+                    return left * right  
+                }
+            }
+        }
+
+        i += 1
+    }
+
+    return -1
+}
+
+let test1 = "(6 * 3)"
+print(evaluate(test1))
